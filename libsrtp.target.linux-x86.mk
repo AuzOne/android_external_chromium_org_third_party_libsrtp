9f8732bd933140c8ac3c865ac2fed948d6d04d55
Android Chromium Automerger <chromium-automerger@android>
2014-03-12 00:41:29 +0000

    Update makefiles after merge of Chromium at 256368
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I7f10fbee169edd78213065c5c2d350d4be487d9a

--- libsrtp.target.linux-x86.mk
+++ libsrtp.target.linux-x86.mk
@@ -59,11 +59,10 @@ MY_CFLAGS_Debug := \
 	-pipe \
 	-fPIC \
 	-Wno-format \
-	-m32 \
-	-mmmx \
-	-march=pentium4 \
 	-msse2 \
 	-mfpmath=sse \
+	-mmmx \
+	-m32 \
 	-fuse-ld=gold \
 	-ffunction-sections \
 	-funwind-tables \
@@ -163,11 +162,10 @@ MY_CFLAGS_Release := \
 	-pipe \
 	-fPIC \
 	-Wno-format \
-	-m32 \
-	-mmmx \
-	-march=pentium4 \
 	-msse2 \
 	-mfpmath=sse \
+	-mmmx \
+	-m32 \
 	-fuse-ld=gold \
 	-ffunction-sections \
 	-funwind-tables \
